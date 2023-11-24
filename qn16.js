c7f5bcc18687903222b698f07b24e37aad24ff5a
pierrine-muhorakeye <pierrinemuhorakeye@gmail.com>
2023-11-24 11:22:50 +0200

    qn16

---
+++ qn16.js
@@ -0,0 +1,13 @@
+// Given an array like this: 
+// var some numbers = [3,4,8,2,1,2,2,6,3,4];
+// Create a function to sort and arrange these elements of the array in ascending order.
+// Modify the first array to make sure that it will also display the last 3 numbers and the first 3 numbers in front of the sorted array
+// From the same sorted array, also display the first 3 numbers.
+function largestValue (arr){
+    var number =arr.sort([3,4,8,2,1,2,2,6,3,4]);
+    return number.largestValue (...arr)    
+}
+concole.log(largestValue([3,4,8,2,1,2,2,6,3,4]));
+
+
+ 
